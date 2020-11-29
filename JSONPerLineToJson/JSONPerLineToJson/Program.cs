314226d7b6770130c11268080ffdd22c0437e1bf
Nadia <nadia@granitewood.com>
2020-11-29 14:48:11 -0600

    Progress Report
    Progress Report

---
+++ JSONPerLineToJson/JSONPerLineToJson/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace JSONPerLineToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var f in Directory.EnumerateFiles("/Users/nadiawood/Documents/GitHub/CourseProject/data"))
+            {
+                if (f.StartsWith(".")) continue;
+                var lines = File.ReadAllLines(f);
+                var output = new List<string>();
+                output.Add("[");
+                output.AddRange(lines.Select(line => line.ToLowerInvariant() + ","));
+
+                string last = output[output.Count - 1];
+                last = last.Substring(0, last.Length - 1);
+                output[output.Count - 1] = last;
+
+                output.Add("]");
+                File.WriteAllLines(f.Replace("/data", "/data/converted"), output);
+            }
+
+        }
+    }
+}
